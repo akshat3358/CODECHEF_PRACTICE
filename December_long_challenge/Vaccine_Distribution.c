9d82ba0d2a725529f162b7523ac13bd054d47d4e
akshat3358 <akshatchaturvedi422@gmail.com>
2020-12-21 19:05:35 +0530

    Vaccine2

---
+++ December_long_challenge/Vaccine_Distribution.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main(){
+    int t;
+    scanf("%d",&t);
+    for(int i=1;i<=t;i++)
+    {
+        int n,d,cnt_tot_days;
+        int cnt_at_risk=0,cnt_not_at_risk=0;
+        scanf("%d %d",&n,&d);
+        for(int j = 1;j<=n;j++)
+        {
+            int age;
+            scanf("%d",&age);
+            if(age<=9 || age>=80)
+            {
+                cnt_at_risk++;
+            }
+            else
+            {
+                cnt_not_at_risk++;
+            }
+        }
+        cnt_tot_days = cnt_at_risk/d + cnt_not_at_risk/d;
+        if(cnt_at_risk % d !=0 )
+        {
+            cnt_tot_days++;
+        }
+        if (cnt_not_at_risk % d != 0)
+        {
+            cnt_tot_days++;
+        }
+        printf("%d",cnt_tot_days);
+    }
+    return 0;
+}
